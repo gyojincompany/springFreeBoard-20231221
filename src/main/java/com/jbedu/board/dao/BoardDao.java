c5e4752fb44ce8ce402e514ab3678f4ec7068468
gyojincompany <luckyred@gmail.com>
2023-12-21 16:30:57 +0900

    BWriteCommand 추가 및 BoardDao write 메서드 추가

--- src/main/java/com/jbedu/board/dao/BoardDao.java
+++ src/main/java/com/jbedu/board/dao/BoardDao.java
@@ -1,5 +1,65 @@
 package com.jbedu.board.dao;
 
-public class BoardDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class BoardDao {
+	
+	DataSource dataSource;
+	
+	public BoardDao() {
+		
+		try {
+			Context context = new InitialContext();
+			context.lookup("java:comp/env/jdbc/oracledb");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+	public void write(String bname, String btitle, String bcontent) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			String sql = "insert into fboard(bid, bname, btitle, bcontent, bhit) values(fboard_seq.nextval,?,?,?,0)";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, bname);
+			pstmt.setString(2, btitle);
+			pstmt.setString(3, bcontent);
+			
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+				
+		}
+		
+	}
+	
 }
